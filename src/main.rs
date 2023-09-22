9311eefafbf95a509510e64cae8b621b8a7b2b5b
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-09-22 10:18:54 -0400

    tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use druid::{Data, widget::{Label, Button, Flex}, Env, Widget, WindowDesc, AppLauncher};
+
+/*
+    * Data
+    * UI
+    * Main
+*/
+
+#[derive(Clone, Data)]
+struct FunnyData {
+    num: i32
+}
+
+fn ui_builder() -> impl Widget<FunnyData> {
+    // Counter
+    let label = Label::new(|data: &FunnyData, _: &Env| format!("Counter: {}", data.num));
+    let increment= Button::new("+")
+        .on_click(|_ctx, data: &mut FunnyData, _env| data.num += 1);
+    let decrement = Button::new("-")
+        .on_click(|_ctx, data: &mut FunnyData, _env| data.num -= 1);
+
+    Flex::column().with_child(label).with_child(Flex::row().with_child(increment).with_child(decrement))
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    //Window Descriptor
+    let main_window = WindowDesc::new(ui_builder())
+        .title("Funny Window");
+    AppLauncher::with_window(main_window)
+        .log_to_console()
+        .launch(FunnyData { num: 0 }).unwrap();
 }
